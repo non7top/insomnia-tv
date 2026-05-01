abd6b6f25d0254e1181d96278b687e296963b22e
non7top <non7top@users.noreply.github.com>
2026-05-01 06:42:10 +0000

    fix(hal): add platform guards to HallSensor

--- src/hal/HallSensor.cpp
+++ src/hal/HallSensor.cpp
@@ -1,20 +1,27 @@
 // Copyright 2026 insomniaTV Contributors. All rights reserved.
 
 #include "HallSensor.h"
+#if defined(ARDUINO)
 #include <Arduino.h>
+#endif
 
 namespace InsomniaTV {
 
 HallSensor::HallSensor(uint8_t pin) : _pin(pin), _zeroPoint(2.5f) {}
 
 float HallSensor::getCurrentAmperes() {
+#if defined(ARDUINO)
     int raw = analogRead(_pin);
     // Basic conversion logic (example)
     float voltage = (raw * 3.3f) / 4095.0f;
     return (voltage - _zeroPoint) / 0.185f; // Example sensitivity 185mV/A
+#else
+    return 0.0f;
+#endif
 }
 
 void HallSensor::calibrate() {
+#if defined(ARDUINO)
     // Simple average over several samples
     float sum = 0;
     for (int i = 0; i < 10; ++i) {
@@ -22,6 +29,7 @@ void HallSensor::calibrate() {
         delay(10);
     }
     _zeroPoint = (sum / 10.0f * 3.3f) / 4095.0f;
+#endif
 }
 
 }  // namespace InsomniaTV
