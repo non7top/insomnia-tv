e65ff58cbab9975d472acf3ed2e5a0e49d300b62
non7top <non7top@users.noreply.github.com>
2026-05-01 10:13:54 +0000

    fix(system): update SystemStateManager constructor with A_HIGH

--- src/system/SystemStateManager.cpp
+++ src/system/SystemStateManager.cpp
@@ -7,10 +7,12 @@
 namespace InsomniaTV {
 
 SystemStateManager::SystemStateManager()
-    : _state({TvPower::ON, Presence::PRESENT, Activity::HIGH}) {
-  EventBus::instance().subscribe(
-      [this](SystemEvent event) { this->handleEvent(event); });
+    : _state({TvPower::ON, Presence::PRESENT, Activity::A_HIGH}) {
+    EventBus::instance().subscribe([this](SystemEvent event) {
+        this->handleEvent(event);
+    });
 }
+
 
 const SystemState& SystemStateManager::getState() const {
   return _state;
@@ -18,24 +20,23 @@ const SystemState& SystemStateManager::getState() const {
 
 void SystemStateManager::handleEvent(SystemEvent event) {
   switch (event) {
-    case SystemEvent::IR_ACTIVITY:
-      _state.activity = Activity::HIGH;
-      break;
-    case SystemEvent::SYSTEM_IDLE:
-      _state.activity = Activity::IDLE;
-      break;
-    case SystemEvent::TV_PING_SUCCESS:
-      _state.tvPower = TvPower::ON;
-      break;
-    case SystemEvent::TV_PING_FAIL:
-      _state.tvPower = TvPower::OFF;
-      break;
-    case SystemEvent::CURRENT_UPDATE:
-      // Logic for future use
-      break;
-    default:
-      break;
+      case SystemEvent::IR_ACTIVITY:
+          _state.activity = Activity::A_HIGH;
+          break;
+      case SystemEvent::SYSTEM_IDLE:
+          _state.activity = Activity::IDLE;
+          break;
+      case SystemEvent::TV_PING_SUCCESS:
+          _state.tvPower = TvPower::ON;
+          break;
+      case SystemEvent::TV_PING_FAIL:
+          _state.tvPower = TvPower::OFF;
+          break;
+      case SystemEvent::CURRENT_UPDATE:
+          // Logic for future use
+          break;
+      default:
+          break;
   }
-}
-
+  }
 }  // namespace InsomniaTV
