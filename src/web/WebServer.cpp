e6aab667e93fcdfa410eaa0579b22076417b16bb
non7top <non7top@users.noreply.github.com>
2026-05-01 09:17:25 +0000

    fix(lint): re-apply clang-format to WebServer and MqttBridge tests

--- src/web/WebServer.cpp
+++ src/web/WebServer.cpp
@@ -1,24 +1,25 @@
 // Copyright 2026 insomniaTV Contributors. All rights reserved.
 
 #include "WebServer.h"
+
 #include <ArduinoJson.h>
 
 namespace InsomniaTV {
 
 #if defined(ARDUINO)
 WebServer::WebServer(uint16_t port) : _server(port) {
-    setupRoutes();
+  setupRoutes();
 }
 
 void WebServer::begin() {
-    _server.begin();
+  _server.begin();
 }
 
 void WebServer::setupRoutes() {
-    _server.on("/api/status", HTTP_GET, [](AsyncWebServerRequest* request) {
-        // Placeholder JSON response
-        request->send(200, "application/json", "{\"status\":\"ok\"}");
-    });
+  _server.on("/api/status", HTTP_GET, [](AsyncWebServerRequest* request) {
+    // Placeholder JSON response
+    request->send(200, "application/json", "{\"status\":\"ok\"}");
+  });
 }
 #else
 WebServer::WebServer(uint16_t port) {}
