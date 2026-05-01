ae8e5b3709fbd688c369675a47064c8ad17430ae
non7top <non7top@users.noreply.github.com>
2026-05-01 06:14:12 +0000

    fix(lint): run clang-format again on system components

--- test/test_system/test_system.cpp
+++ test/test_system/test_system.cpp
@@ -1,7 +1,9 @@
 // Copyright 2026 insomniaTV Contributors. All rights reserved.
 
 #include <unity.h>
+
 #include <vector>
+
 #include "../../src/system/EventBus.h"
 #include "../../src/system/SystemStateManager.h"
 
@@ -11,33 +13,33 @@ using InsomniaTV::SystemEvent;
 using InsomniaTV::SystemStateManager;
 
 void setUp() {
-    EventBus::instance().clear();
+  EventBus::instance().clear();
 }
 
 void test_event_bus_subscription() {
-    bool event_received = false;
-    EventBus::instance().subscribe([&event_received](SystemEvent e) {
-        if (e == SystemEvent::SYSTEM_IDLE) {
-            event_received = true;
-        }
-    });
-    EventBus::instance().publish(SystemEvent::SYSTEM_IDLE);
-    TEST_ASSERT_TRUE(event_received);
+  bool event_received = false;
+  EventBus::instance().subscribe([&event_received](SystemEvent e) {
+    if (e == SystemEvent::SYSTEM_IDLE) {
+      event_received = true;
+    }
+  });
+  EventBus::instance().publish(SystemEvent::SYSTEM_IDLE);
+  TEST_ASSERT_TRUE(event_received);
 }
 
 void test_system_state_manager_update() {
-    SystemStateManager ssm;
+  SystemStateManager ssm;
 
-    EventBus::instance().publish(SystemEvent::IR_ACTIVITY);
-    TEST_ASSERT_EQUAL(Activity::HIGH, ssm.getState().activity);
+  EventBus::instance().publish(SystemEvent::IR_ACTIVITY);
+  TEST_ASSERT_EQUAL(Activity::HIGH, ssm.getState().activity);
 
-    EventBus::instance().publish(SystemEvent::SYSTEM_IDLE);
-    TEST_ASSERT_EQUAL(Activity::IDLE, ssm.getState().activity);
+  EventBus::instance().publish(SystemEvent::SYSTEM_IDLE);
+  TEST_ASSERT_EQUAL(Activity::IDLE, ssm.getState().activity);
 }
 
 int main() {
-    UNITY_BEGIN();
-    RUN_TEST(test_event_bus_subscription);
-    RUN_TEST(test_system_state_manager_update);
-    return UNITY_END();
+  UNITY_BEGIN();
+  RUN_TEST(test_event_bus_subscription);
+  RUN_TEST(test_system_state_manager_update);
+  return UNITY_END();
 }
