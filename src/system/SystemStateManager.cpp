425740fce553835991ad033ef5efb0ea37e67704
non7top <non7top@users.noreply.github.com>
2026-05-01 14:26:59 +0000

    fix(lint): re-apply clang-format to SystemStateManager.cpp and test_system.cpp

--- src/system/SystemStateManager.cpp
+++ src/system/SystemStateManager.cpp
@@ -6,40 +6,37 @@
 
 namespace InsomniaTV {
 SystemStateManager::SystemStateManager() {
-    _state.tvPower = TvPower::ON;
-    _state.presence = Presence::PRESENT;
-    _state.activity = InsomniaTV::Activity::HIGH;
-    EventBus::instance().subscribe([this](SystemEvent event) {
-        this->handleEvent(event);
-    });
+  _state.tvPower = TvPower::ON;
+  _state.presence = Presence::PRESENT;
+  _state.activity = InsomniaTV::Activity::HIGH;
+  EventBus::instance().subscribe(
+      [this](SystemEvent event) { this->handleEvent(event); });
 }
-
 
 const SystemState& SystemStateManager::getState() const {
   return _state;
 }
 
 void SystemStateManager::handleEvent(SystemEvent event) {
-    switch (event) {
-        case SystemEvent::IR_ACTIVITY:
-            _state.activity = InsomniaTV::Activity::HIGH;
-            break;
-        case SystemEvent::SYSTEM_IDLE:
-            _state.activity = InsomniaTV::Activity::IDLE;
-            break;
-        case SystemEvent::TV_PING_SUCCESS:
-            _state.tvPower = TvPower::ON;
-            break;
-        case SystemEvent::TV_PING_FAIL:
-            _state.tvPower = TvPower::OFF;
-            break;
-        case SystemEvent::CURRENT_UPDATE:
-            // Logic for future use
-            break;
-        default:
-            break;
-    }
+  switch (event) {
+    case SystemEvent::IR_ACTIVITY:
+      _state.activity = InsomniaTV::Activity::HIGH;
+      break;
+    case SystemEvent::SYSTEM_IDLE:
+      _state.activity = InsomniaTV::Activity::IDLE;
+      break;
+    case SystemEvent::TV_PING_SUCCESS:
+      _state.tvPower = TvPower::ON;
+      break;
+    case SystemEvent::TV_PING_FAIL:
+      _state.tvPower = TvPower::OFF;
+      break;
+    case SystemEvent::CURRENT_UPDATE:
+      // Logic for future use
+      break;
+    default:
+      break;
+  }
 }
-
 
 }  // namespace InsomniaTV
